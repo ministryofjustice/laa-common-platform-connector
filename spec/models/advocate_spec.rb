d4c16a3242e317e0d7d61a630d281cbf5506f932
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-02-20 16:16:04 +0000

    Add basic validation spec for Advocate model

--- spec/models/advocate_spec.rb
+++ spec/models/advocate_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Advocate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'basic validation' do
+    it { should validate_presence_of(:first_name)}
+    it { should validate_presence_of(:last_name)}
+    it { should validate_presence_of(:organisation_name)}
+    it { should validate_presence_of(:status)}
+  end
 end
