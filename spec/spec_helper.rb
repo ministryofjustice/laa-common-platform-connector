bcaefdcbabf181aad49b35698617730014643e3a
Katherine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2019-02-21 14:56:17 +0000

    Include missing files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,11 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'webmock/rspec'
+require_relative './support/fake_common_platform'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -93,4 +98,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.before(:each) do
+    stub_request(:any, /api.common-platform.gov/).to_rack(FakeCommonPlatform)
+  end
 end
