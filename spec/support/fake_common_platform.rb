3ce4c380263f36284c5b289ca3df51ad9beff868
Katherine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2019-02-21 14:38:42 +0000

    Added fake endpoint for testing common platform api

---
+++ spec/support/fake_common_platform.rb
@@ -0,0 +1,16 @@
+# spec/support/fake_github.rb
+require 'sinatra/base'
+
+class FakeCommonPlatform < Sinatra::Base
+  get '/hearings/:id' do
+    json_response 200, 'hearing.events.hearing-resulted-convictionAtTrial.json'
+  end
+
+  private
+
+  def json_response(response_code, file_name)
+    content_type :json
+    status response_code
+    File.open(File.dirname(__FILE__) + '/fixtures/' + file_name, 'rb').read
+  end
+end
