eb9712b67e63fb3d8363cdc8daa42ddccb28ef1c
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-02-22 17:04:05 +0000

    Allows you to hit /refreshdata and populate the db
    Currently only parses a hearing and its defendants
    To add them to the db.

--- app/controllers/refresh_data_controller.rb
+++ app/controllers/refresh_data_controller.rb
@@ -1,6 +1,12 @@
 class RefreshDataController < ApplicationController
   def call_common_platform
     data =  CommonPlatformRequestService.call("http://localhost:4567/hearings/1")
-    render :json => data
+    hearing = ConverterService.map(data)
+    if hearing.valid?
+      hearing.save!
+      render plain: 'data updated'
+    else
+      render plain: 'an error occurd'
+    end
   end
 end
