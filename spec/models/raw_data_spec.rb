5ea6e002ca6f56dc0aeab3cdcee9c1381320fa21
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-02-25 16:23:24 +0000

    Add spec for RawData model

--- spec/models/raw_data_spec.rb
+++ spec/models/raw_data_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe RawData, type: :model do
     it { should validate_presence_of(:common_platform_uuid)}
     it { should validate_presence_of(:common_platform_json)}
   end
+
+  it 'can store and retrieve json in the right format' do
+    test_json = {name: 'jim', job: 'painter' }.to_json
+
+    rd = RawData.create(common_platform_uuid: '1', common_platform_json: test_json)
+
+    expect(rd.common_platform_json).to match(test_json)
+  end
 end
