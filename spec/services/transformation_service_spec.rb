6fc29880b26d63d4868bc58e4cda67b6d870f4f3
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-02-28 15:26:22 +0000

    Add transformation service
    Adds service to transfrom the common platform
    data from large json to smaller json.

--- spec/services/transformation_service_spec.rb
+++ spec/services/transformation_service_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe TransformationService do
   before :each do
-    @data = read_test_json('hearing.events.hearing-resulted-convictionAtTrial.json')
-    @data_transformed = read_test_json('hearing.events.hearing-resulted-convictionAtTrial-transformed.json')
+    raw_data_path = 'hearing.events.hearing-resulted-convictionAtTrial.json'
+    test_data_path = 'hearing.events.hearing-resulted-convictionAtTrial-transformed.json'
+    @data = read_test_json(raw_data_path)
+    @test_transformed_data = read_test_json(test_data_path)
   end
 
   it 'can convert common platform json' do
-    tranformed_data = TransformationService.transform(@data)
-    expect(transformed_data).to eq(@data_transformed)
+    transformed_data = TransformationService.transform(@data)
+    expect(transformed_data).to eq(@test_transformed_data.to_json)
   end
 end
