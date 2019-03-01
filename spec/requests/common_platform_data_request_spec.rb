9ddaad8dfb3c43a905226ac2c257422a6b388005
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-03-01 13:25:59 +0000

    Fix failing test
    JSON.parse in test was failing because double
    quotes were surrounding the test JSON string.
    
    Changed to single quotes and double quotes for JSON
    values.

--- spec/requests/common_platform_data_request_spec.rb
+++ spec/requests/common_platform_data_request_spec.rb
@@ -4,7 +4,9 @@ require 'rails_helper'
 
 RSpec.describe 'CommonPlatformData', type: :request do
   before do
-    @common_platform_datum = CommonPlatformData.create!(common_platform_uuid:'57da63d6-39e1-43da-8c9d-46ba49800825', common_platform_json:"{'Hearing':'Test hearing Data'}", transformed_common_platform_json:"{'Hearing':'Test hearing Data Transformed'}")
+    @common_platform_datum = CommonPlatformData.create!(common_platform_uuid:'57da63d6-39e1-43da-8c9d-46ba49800825',
+                                                        common_platform_json: '{"hearing":"Test hearing Data"}',
+                                                        transformed_common_platform_json: '{ "hearing" : "Test hearing Data Transformed"}')
   end
 
   describe 'requesting a transformed common platform data record' do
@@ -13,7 +15,7 @@ RSpec.describe 'CommonPlatformData', type: :request do
       json = JSON.parse(response.body)
       expect(response).to have_http_status(200)
       expect(json).not_to be_empty
-      expect(json['common_platform_uuid']).to eq(@common_platform_datum.common_platform_uuid) 
+      expect(json['hearing']).to eq("Test hearing Data Transformed")
     end
   end
 end
