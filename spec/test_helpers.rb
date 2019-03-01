ab4a99ec29aa3710d4b532643e87fdb0bf947a16
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-03-01 09:41:27 +0000

    Refactor transformation service
    Transformation service now accepts a Transformer
    which actually performs the transformation.
    
    This should allow us to more easily vary the algorithm
    that is needed to parse and transform the JSON as
    we start to understand more about the what is required.

--- spec/test_helpers.rb
+++ spec/test_helpers.rb
@@ -4,3 +4,10 @@ def read_test_json(filename)
     @json = File.open(@filepath, 'rb').read
     JSON.parse(@json)
 end
+
+def setup_test_json
+  raw_data_path = 'hearing.events.hearing-resulted-convictionAtTrial.json'
+  test_data_path = 'hearing.events.hearing-resulted-convictionAtTrial-transformed.json'
+  @data = read_test_json(raw_data_path)
+  @test_transformed_data = read_test_json(test_data_path)
+end
