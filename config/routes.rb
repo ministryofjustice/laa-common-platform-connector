9b6a5b0e9bfaf10330be752ffee9cdf2da52ef2e
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-03-01 11:23:06 +0000

    Restrict routes
    Stop resources generating uneeded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/refreshdata' => 'refresh_data#call_common_platform'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :common_platform_data
+  resources :common_platform_data, only: [:show]
 end
