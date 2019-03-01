da431963d4c3b87ecdb5274af9d61f112c7b5a4b
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-03-01 12:42:45 +0000

    Update re data controller
    Update the requestdata controller to
    work with new TransformationService

--- app/controllers/refresh_data_controller.rb
+++ app/controllers/refresh_data_controller.rb
@@ -1,9 +1,12 @@
 class RefreshDataController < ApplicationController
   def call_common_platform
     data =  CommonPlatformRequestService.call("http://localhost:4567/hearings/1")
-    hearing = ConverterService.map(data)
-    if hearing.valid?
-      hearing.save!
+    transformedData = TransformationService.new(BasicTransformer.new).transform(data)
+    commonPlatformData = CommonPlatformData.new(common_platform_uuid: '1',
+                                                common_platform_json: data,
+                                                transformed_common_platform_json: transformedData)
+    if commonPlatformData.valid?
+      commonPlatformData.save!
       render plain: 'data updated'
     else
       render plain: 'an error occurd'
