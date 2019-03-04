04e0268caa39bbbbb6170b75ec34ac302a987591
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-03-04 12:00:31 +0000

    Change fake data api route
    Also as part of this tests now reference ENV variables rather than hard coded
    strings.
    
    The fake api route as /hearings/:id. This has been changed to /hearing/:id, as
    this is more RESTful.

--- spec/services/common_platform_request_service_spec.rb
+++ spec/services/common_platform_request_service_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CommonPlatformRequestService do
-  let(:url){ 'https://api.common-platform.gov/hearings/1' }
 
   describe 'get data from mockservice' do
     it 'calls mock service and returns an openstruct od data' do
-      @openstruct = CommonPlatformRequestService.call(url)
+      @openstruct = CommonPlatformRequestService.call(ENV['REFRESH_DATA_API'])
       expect(@openstruct).to be_an_instance_of(OpenStruct)
       expect(@openstruct.hearing.courtCentre.name).to eq('Wood Green Crown Court')
     end
