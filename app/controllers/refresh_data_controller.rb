47652caa9cbdb316c9f63b0c42cd0947615f5185
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-03-04 11:43:35 +0000

    Externalise URLs
    Adds Dotenv gem to externalise environment
    variables.

--- app/controllers/refresh_data_controller.rb
+++ app/controllers/refresh_data_controller.rb
@@ -1,6 +1,6 @@
 class RefreshDataController < ApplicationController
   def call_common_platform
-    data =  CommonPlatformRequestService.call("http://localhost:4567/hearings/1")
+    data =  CommonPlatformRequestService.call(ENV['REFRESH_DATA_API'])
     transformedData = TransformationService.new(BasicTransformer.new).transform(data)
     commonPlatformData = CommonPlatformData.new(common_platform_uuid: '1',
                                                 common_platform_json: data,
